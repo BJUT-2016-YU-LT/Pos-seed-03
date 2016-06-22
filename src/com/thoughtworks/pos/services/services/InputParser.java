c0f99a8c85ef673fdd3016a5e11bf50a6a381d4a
ivanlu2013 <835498692@qq.com>
2016-06-22 23:12:43 +0800

    delete old & useless code

--- src/com/thoughtworks/pos/services/services/InputParser.java
+++ src/com/thoughtworks/pos/services/services/InputParser.java
@@ -35,8 +35,6 @@ public class InputParser{
     private ShoppingChart BuildShoppingChart(ArrayList<Item> items) {
         ShoppingChart shoppingChart = new ShoppingChart();
         for (Item item : items) {
-            //Item mappedItem = itemIndexes.get(barcode);
-            //Item item = new Item(barcode, mappedItem.getName(), mappedItem.getUnit(), mappedItem.getPrice(), mappedItem.getDiscount());
             shoppingChart.add(item);
         }
         return shoppingChart;
@@ -48,52 +46,3 @@ public class InputParser{
         return objectMapper.readValue(itemsIndexStr, typeRef);
     }
 }
-
-/*
-the following is seed file copy form teacher
-now is useless
- */
-/**
- * Created by Administrator on 2015/1/2.
- */
-/*
-public class InputParser {
-    private File indexFile;
-    private File itemsFile;
-    private final ObjectMapper objectMapper;
-
-    public InputParser(File indexFile, File itemsFile) {
-        this.indexFile = indexFile;
-        this.itemsFile = itemsFile;
-        objectMapper = new ObjectMapper(new JsonFactory());
-        objectMapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
-    }
-
-    public ShoppingChart parser() throws IOException {
-        return BuildShoppingChart(getBoughtItemBarCodes(), getItemIndexes());
-    }
-
-    private ShoppingChart BuildShoppingChart(String[] barCodes, HashMap<String, Item> itemIndexes) {
-        ShoppingChart shoppingChart = new ShoppingChart();
-        for (String barcode : barCodes) {
-            Item mappedItem = itemIndexes.get(barcode);
-            Item item = new Item(barcode, mappedItem.getName(), mappedItem.getUnit(), mappedItem.getPrice(), mappedItem.getDiscount());
-            shoppingChart.add(item);
-        }
-        return shoppingChart;
-    }
-
-    private String[] getBoughtItemBarCodes() throws IOException {
-        String itemsStr = FileUtils.readFileToString(itemsFile);
-        return objectMapper.readValue(itemsStr, String[].class);
-    }
-
-    private HashMap<String, Item> getItemIndexes() throws IOException {
-        String itemsIndexStr = FileUtils.readFileToString(indexFile);
-        TypeReference<HashMap<String,Item>> typeRef = new TypeReference<HashMap<String,Item>>() {};
-        return objectMapper.readValue(itemsIndexStr, typeRef);
-    }
-
-}
-
- */
